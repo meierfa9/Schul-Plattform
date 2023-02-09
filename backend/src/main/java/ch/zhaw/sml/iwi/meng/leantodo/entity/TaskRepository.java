8cd1b8551c797ce10c972c3cc34f7da43dad37b0
Fabio Meier <meierfa9@students.zhaw.ch>
2023-02-09 15:28:28 +0100

    Security

--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/TaskRepository.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/entity/TaskRepository.java
@@ -1,7 +1,17 @@
 package ch.zhaw.sml.iwi.meng.leantodo.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.annotation.Secured;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
-    
+    /*
+    @Override
+    @Secured("ROLE_TEACHER")
+    <S extends Task> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long id);
+    */
 }
--- backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/SecurityConfig.java
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/security/SecurityConfig.java
@@ -19,57 +19,59 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
-    @Bean
-    public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("meierfa9")
-                .password(bCryptPasswordEncoder.encode("meierfa9pass"))
-                .roles("Student")
-                .build());
-        manager.createUser(User.withUsername("mettlanj")
-                .password(bCryptPasswordEncoder.encode("mettlanjpass"))
-                .roles("Student")
-                .build());
-        manager.createUser(User.withUsername("beckema2")
-                .password(bCryptPasswordEncoder.encode("beckema2pass"))
-                .roles("Student")
-                .build());
-        manager.createUser(User.withUsername("heip")
-                .password(bCryptPasswordEncoder.encode("heippass"))
-                .roles("Student", "Teacher")
-                .build());
-        manager.createUser(User.withUsername("meix")
-                .password(bCryptPasswordEncoder.encode("meixpass"))
-                .roles("Student", "Teacher")
-                .build());
-        return manager;
-    }
+        @Bean
+        public UserDetailsService userDetailsService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+                InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+                manager.createUser(User.withUsername("meierfa9")
+                                .password(bCryptPasswordEncoder.encode("meierfa9pass"))
+                                .roles("STUDENT")
+                                .build());
+                manager.createUser(User.withUsername("mettlanj")
+                                .password(bCryptPasswordEncoder.encode("mettlanjpass"))
+                                .roles("STUDENT")
+                                .build());
+                manager.createUser(User.withUsername("beckema2")
+                                .password(bCryptPasswordEncoder.encode("beckema2pass"))
+                                .roles("STUDENT")
+                                .build());
+                manager.createUser(User.withUsername("heip")
+                                .password(bCryptPasswordEncoder.encode("heippass"))
+                                .roles("STUDENT", "TEACHER")
+                                .build());
+                manager.createUser(User.withUsername("meix")
+                                .password(bCryptPasswordEncoder.encode("meixpass"))
+                                .roles("STUDENT", "TEACHER")
+                                .build());
+                return manager;
+        }
 
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
-            UserDetailsService userDetailsService)
-            throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder)
-                .and()
-                .build();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(HttpSecurity http,
+                        BCryptPasswordEncoder bCryptPasswordEncoder,
+                        UserDetailsService userDetailsService)
+                        throws Exception {
+                return http.getSharedObject(AuthenticationManagerBuilder.class)
+                                .userDetailsService(userDetailsService)
+                                .passwordEncoder(bCryptPasswordEncoder)
+                                .and()
+                                .build();
+        }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers(toH2Console()).permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic().and()
-                .csrf().ignoringRequestMatchers(toH2Console()).and()
-                .headers().frameOptions().sameOrigin();
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.authorizeHttpRequests()
+                                .requestMatchers(toH2Console()).permitAll()
+                                .anyRequest().authenticated()
+                                .and().httpBasic().and()
+                                .csrf().ignoringRequestMatchers(toH2Console()).and()
+                                .headers().frameOptions().sameOrigin();
+                http.csrf().disable();
+                return http.build();
+        }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
 }
