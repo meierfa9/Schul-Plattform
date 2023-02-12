84235064b2ee50bca3aad44bdcfe584ea7ebb655
beckema2 <beckema2@students.zhaw.ch>
2023-02-12 20:31:28 +0100

    Login, Aufgabe hinzufügen CSS

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,21 +2,29 @@
   <ion-page>
     <ion-content :fullscreen="true" padding>
       <ion-grid>
-        <ion-row justify-content-center>
-          <ion-col align-self-center size-md="6" size-lg="5" size-xs="12">
-            <div text-center>
-              <h1>Schulplattform Login</h1>
-            </div>
-            <div padding>
-              <ion-item>
-                <ion-input type="text" placeholder="Benutzername" v-model="username"></ion-input>
-              </ion-item>
-              <ion-item>
-                <ion-input type="password" placeholder="Passwort" v-model="password"></ion-input>
-              </ion-item>
-            </div>
-            <div padding>
-              <ion-button size="large" @click="login" expand="block">Login</ion-button>
+        <ion-row>
+          <ion-col>
+            <div class="container">
+              <ion-card class="card">
+                <ion-card-header>
+                  <ion-card-title>Schulplattform
+                    <img alt="logo" src="https://cdn-icons-png.flaticon.com/128/2602/2602414.png" />
+                  </ion-card-title>
+                  <ion-card-subtitle>Login</ion-card-subtitle>
+                </ion-card-header>
+
+                <ion-card-content>
+                  <div padding>
+                    <ion-item>
+                      <ion-input type="text" placeholder="Benutzername" v-model="username"></ion-input>
+                    </ion-item>
+                    <ion-item>
+                      <ion-input type="password" placeholder="Passwort" v-model="password"></ion-input>
+                    </ion-item>
+                  </div>
+                </ion-card-content>
+                  <ion-button class="button" @click="login">Login</ion-button>
+              </ion-card>
             </div>
           </ion-col>
         </ion-row>
@@ -27,6 +35,7 @@
 
 <script setup lang="ts">
 import {
+  IonCard,
   IonPage,
   IonContent,
   IonInput,
@@ -41,3 +50,44 @@ import { useLogin } from "../composables/useLogin";
 const { username, password, login } = useLogin();
 
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+}
+
+.card {
+  text-align: left;
+  align-self: center;
+  width: 60%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.503);
+}
+
+.card:hover {
+  --background: rgba(211, 211, 211, 0.38);
+}
+
+
+img {
+  display: right;
+  width: 5%;
+  position: absolute;
+        top: 10px;
+        right: 20px;
+}
+
+.button {
+  color: black;
+  --background: lightgrey;
+  padding: 5px 30px;
+}
+.button:hover {
+  padding: 5px 30px;
+  --background: rgb(0, 106, 255);
+  color: white;
+}
+
+</style>
--- frontend/src/views/NewPage.vue
+++ frontend/src/views/NewPage.vue
@@ -8,61 +8,64 @@
         <ion-title>Aufgabe hinzufügen</ion-title>
       </ion-toolbar>
     </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Aufgabe hinzufügen</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-list>
-        <ion-item>
-          <ion-grid>
-            <ion-row>
-              <ion-text>Titel der Aufgabe</ion-text>
-            </ion-row>
-            <ion-row>
-              <ion-input type="text" placeholder="Titel" v-model="newTask.name"></ion-input>
-            </ion-row>
-          </ion-grid>
-          </ion-item>
-          <ion-item>
-          <ion-grid>
-            <ion-row>
-              <ion-text>Beschreibung der Aufgabe</ion-text>
-            </ion-row>
-            <ion-row>
-              <ion-input type="text" placeholder="Beschreibung" v-model="newTask.description"></ion-input>
-            </ion-row>
-          </ion-grid>
-          </ion-item>
-        
-          <ion-item>
-          <ion-grid>
-            <ion-row>
-              <ion-text>Schulfach der Aufgabe</ion-text>
-            </ion-row>
-            <ion-row>
-              <ion-select type="number" placeholder="Beschreibung" v-model="newTask.title">
-                <ion-select-option :key="fach.title" v-for="fach in faecher">{{ fach.title }}</ion-select-option>
-              </ion-select>
-            </ion-row>
-          </ion-grid>
-          </ion-item>
-
-
-
-
-
-
-          
-      </ion-list>
-      <div padding>
-        <ion-button @click="addTask()">Aufgabe hinzufügen</ion-button>
+      <div class="container">
+        <ion-card class="card">
+          <ion-card-header class="header">
+            <ion-card-title>neue Aufgabe
+              <img class="new" alt="logo" src="https://cdn-icons-png.flaticon.com/512/891/891509.png" />
+            </ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-list>
+              <ion-item>
+                <ion-grid>
+                  <ion-row>
+                    <ion-text>
+                      <img class="icon" alt="logo" src="https://cdn-icons-png.flaticon.com/512/2799/2799932.png" />
+                      Titel der Aufgabe</ion-text>
+                  </ion-row>
+                  <ion-row>
+                    <ion-input type="text" placeholder="Titel" v-model="newTask.name"></ion-input>
+                  </ion-row>
+                </ion-grid>
+              </ion-item>
+              <ion-item>
+                <ion-grid>
+                  <ion-row>
+                    <ion-text>
+                      <img class="icon" alt="logo" src="https://cdn-icons-png.flaticon.com/512/5107/5107639.png" />
+                      Beschreibung der Aufgabe</ion-text>
+                  </ion-row>
+                  <ion-row>
+                    <ion-input type="text" placeholder="Beschreibung" v-model="newTask.description"></ion-input>
+                  </ion-row>
+                </ion-grid>
+              </ion-item>
+              <ion-item>
+                <ion-grid>
+                  <ion-row>
+                    <ion-text>
+                      <img class="icon" alt="logo" src="https://cdn-icons-png.flaticon.com/128/8895/8895467.png" />
+                      Schulfach der Aufgabe</ion-text>
+                  </ion-row>
+                  <ion-row>
+                    <ion-select type="number" placeholder="Beschreibung" v-model="newTask.title">
+                      <ion-select-option :key="fach.title" v-for="fach in faecher">{{ fach.title }}</ion-select-option>
+                    </ion-select>
+                  </ion-row>
+                </ion-grid>
+              </ion-item>
+            </ion-list>
+          </ion-card-content>
+          <ion-button @click="addTask()">Aufgabe hinzufügen</ion-button>
+        </ion-card>
       </div>
     </ion-content>
   </ion-page>
 </template>
+
+
 
 <script setup lang="ts">
 import {
@@ -84,7 +87,7 @@ import { useTasks } from "../composables/useAufgaben";
 import { useFaecher } from "../composables/useFaecher";
 
 
-const { tasks, newTask, addTask, finishTask} =
+const { tasks, newTask, addTask, finishTask } =
   useTasks();
 
 const { faecher, lehrer } = useFaecher();
@@ -92,8 +95,55 @@ const { faecher, lehrer } = useFaecher();
 </script>
 
 <style>
-  ion-textarea {
+ion-textarea {
   background-color: lightgrey;
   color: black;
 }
+
+.new {
+  display: right;
+  width: 5%;
+  position: absolute;
+        right: 20px;
+}
+
+.icon {
+  display: right;
+  width: 3%;
+  position: absolute;
+        right: 600px;
+        top: 5px;
+}
+
+.header {
+  padding: 25px;
+}
+
+.card {
+  text-align: left;
+  align-self: center;
+  width: 60%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.503);
+}
+
+.card:hover {
+  --background: rgba(211, 211, 211, 0.38);
+}
+
+.container {
+  display: flex;
+  height: 450px;
+}
+
+.button {
+  color: black;
+  --background: lightgrey;
+  padding: 5px 30px;
+}
+.button:hover {
+  padding: 5px 30px;
+  --background: rgb(0, 106, 255);
+  color: white;
+}
+
 </style>
